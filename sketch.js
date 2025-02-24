03abee91ba59c2f65bdedc15bc6b06145bb5adc4
matheus <matheus.gualberto.freitas@escola.pr.gov.br>
2025-02-24 11:09:25 -0300

    24/02/2025

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  //Função draw significa desenhar
+  function draw() {
+    background(34, 139, 34);
+    circle(50, 50, 100);
+    circle(150, 50, 100);
+    circle(250, 50, 100);
+    circle(350, 50, 100);
+    
+    circle(50, 350, 100);
+    circle(150, 350, 100);
+    circle(250, 350, 100);
+    circle(350, 350, 100);
+  }
